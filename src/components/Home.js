c9c11ec3d8c2ec0d110128af7ea5e3ca60ff32fc
laxmi Tiruvuri <latiruvu@microsoft.com>
2024-09-16 14:49:03 -0700

    add these files

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Home(){
+
+    return (
+        <div>
+            <h1>Welcome to Feeds Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,50 @@
+import React, { useState }  from "react";
+import { useNavigate } from "react-router-dom";
+
+
+function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        //handle login logic here
+        console.log('Logging in with', { email, password})
+        try {
+            //const response = await axios.post('/api/login', { email, password });
+           // localStorage.setItem('token', response.data.token);
+            navigate('/home');
+          } catch (error) {
+            setError('Invalid credentials');
+          }
+        // // On successful login, navigate to the homepage
+        // navigate('/');
+    }
+    return(
+        <div>
+            <h1>Welcome to Login Page</h1>
+            <form onSubmit={handleLogin}>
+                <div>
+                <label>Email Id:</label>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <button type="submit">Login</button>
+                {error && <p>{error}</p>}
+            </form>
+        </div>
+    );
+};
+
+export default Login;
