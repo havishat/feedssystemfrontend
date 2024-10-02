4dfbaa408c2e77414f7cc623f188c8a640ca40a1
laxmi Tiruvuri <latiruvu@microsoft.com>
2024-10-01 17:00:12 -0700

    changed the routes libaray

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import RegistrationPage from './components/RegistrationPage'
 
 function App() {
   return (
     <div className="App">
       <Router basename={process.env.PUBLIC_URL}>
-        <Routes>
+        <Switch>
         <Route path="/register" component={<RegistrationPage />} />
         <Route path="/login" component={<LoginPage />} />
         <Route path="/home" component={<HomePage />} />
         <Route path="/" component={<LoginPage />} />
-        </Routes>
+        </Switch>
       </Router>
     </div>
   );
